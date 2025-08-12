09698256854564d540cc82888fca967a7016a4b1
TDkRecords <tdksongs@gmail.com>
2025-08-12 08:47:08 -0500

    error de importacion en auth.js

--- src/lib/assets/js/auth.js
+++ src/lib/assets/js/auth.js
@@ -1,4 +1,4 @@
-import { auth, db } from "$lib/assets/jS/firebase";
+import { auth, db } from "$lib/assets/js/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 
